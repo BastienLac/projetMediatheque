2205fec08405ef4504197351aa94c18f5bba5e29
Bastien LACOUR <blacour@lerebours.org>
2022-11-26 15:07:00 +0100

    Initial commit

---
+++ src/MySQLConnection.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class MySQLConnection {
+    public static String lien = "jdbc:mysql://localhost:3306/mediatheque";
+    public static String utilisateur = "root";
+    public static Connection getConnexion(){
+        try
+        {
+            //étape 1: charger la classe de driver
+            Class.forName("com.mysql.jdbc.Driver");
+            //étape 2: créer l'objet de connexion
+            Connection conn = DriverManager.getConnection(
+                    MySQLConnection.lien, utilisateur, "");
+
+            return conn;
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+}
