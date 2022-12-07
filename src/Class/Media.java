5a6cf085f2c1b48d15c37f87bc559489205a9b55
MohamadRESLAN <mohamadhamzereslan@hotmail.com>
2022-12-07 18:58:53 +0100

    b

---
+++ src/Class/Media.java
@@ -0,0 +1,13 @@
+package Class;
+
+public abstract class Media {
+    protected int id;
+    protected String crateur;
+    protected String titre;
+    protected double prixHT;
+    protected String auteurRealisateurInterprete;
+    protected int dureeNbPagesNbChansons;
+    protected int anneeDeParution;
+    protected int nombrePage;
+    protected int nombreChanson;
+}
