d0e5257638682b4637adc449c77d4335e2fec9b6
MohamadRESLAN <mohamadhamzereslan@hotmail.com>
2022-12-11 20:40:31 +0100

    ...

--- src/ConnexionBoutonListener.java
+++ src/ConnexionBoutonListener.java
@@ -10,31 +10,31 @@ public class ConnexionBoutonListener implements ActionListener {
     static JTextField txtMdp = new JPasswordField(16);
     static JFrame jf = new JFrame("Connexion");
     public static void main(){
-            JPanel connexionPanel = new JPanel();
-            connexionPanel.setLayout(null);
-            jf.setSize(500,500);
+        JPanel connexionPanel = new JPanel();
+        connexionPanel.setLayout(null);
+        jf.setSize(500,500);
 
-            JLabel connexionLabel = new JLabel("Bonjour, veuillez vous connecter", JLabel.CENTER);
-            connexionLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-            connexionLabel.setBounds(80,30, 300, 40);
+        JLabel connexionLabel = new JLabel("Bonjour, veuillez vous connecter", JLabel.CENTER);
+        connexionLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        connexionLabel.setBounds(80,30, 300, 40);
 
-            ConnexionBoutonListener cbl = new ConnexionBoutonListener();
-            connexionButton.addActionListener(cbl);
-            txtLogin.setBounds(130,100, 200, 40);
-            txtMdp.setBounds(130,150, 200, 40);
-            connexionButton.setBounds(130, 200, 200, 40);
-            inscriptionButton.setBounds(20, 400, 150, 30);
-            inscriptionButton.addActionListener(cbl);
+        ConnexionBoutonListener cbl = new ConnexionBoutonListener();
+        connexionButton.addActionListener(cbl);
+        txtLogin.setBounds(130,100, 200, 40);
+        txtMdp.setBounds(130,150, 200, 40);
+        connexionButton.setBounds(130, 200, 200, 40);
+        inscriptionButton.setBounds(20, 400, 150, 30);
+        inscriptionButton.addActionListener(cbl);
 
-            connexionPanel.add(connexionLabel);
-            connexionPanel.add(txtLogin);
-            connexionPanel.add(txtMdp);
-            connexionPanel.add(connexionButton);
-            connexionPanel.add(inscriptionButton);
-            jf.add(connexionPanel);
-            jf.setVisible(true);
-            jf.setLocationRelativeTo(null);
-            jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        connexionPanel.add(connexionLabel);
+        connexionPanel.add(txtLogin);
+        connexionPanel.add(txtMdp);
+        connexionPanel.add(connexionButton);
+        connexionPanel.add(inscriptionButton);
+        jf.add(connexionPanel);
+        jf.setVisible(true);
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public void actionPerformed(ActionEvent e){
         String s = e.getActionCommand();
@@ -61,7 +61,7 @@ public class ConnexionBoutonListener implements ActionListener {
             else
                 txtLogin.setText("TES PAS CO");
         } else if (s.equals("S'inscrire")) {
-             InscriptionPage.main();
+            InscriptionPage.main();
         } else if (s.equals("Valider")) {
             Utilisateur.inscrireUtilisateur();
         }
